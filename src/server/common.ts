15bf80f9cfaa5aa34013a2d273471cb94a182f1a
Gadi Cohen <dragon@wastelands.net>
2023-09-17 13:19:41 +0100

    chore(server/common): remove makeRelay from stuck vscode sesssion

--- src/server/common.ts
+++ src/server/common.ts
@@ -92,16 +92,3 @@ export async function processDbRequest(
 
   return response;
 }
-
-export default function makeRelay(db: Db, relayPassword?: string) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return async function expressRelay(req: Request, res: any) {
-    const response = await processDbRequest(db, req, relayPassword);
-
-    res.status(response.status);
-    for (const [key, value] of response.headers.entries())
-      if ("setHeader" in res) res.setHeader(key, value);
-      else res.headers.set(key, value);
-    res.end(response.body);
-  };
-}
